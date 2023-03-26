65542a64fbf9d605d75f1909e74bb54b7c927e7e
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-03-26 16:38:47 +0200

    Initial commit

---
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,18 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+        
+        printFromNumberToOne(9);
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        int i = number;
+        while(i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+
+}
