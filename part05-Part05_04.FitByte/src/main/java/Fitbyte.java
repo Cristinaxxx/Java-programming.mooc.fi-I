db3050540f71254fa71748a41bc48a3cd0b225a7
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-03-26 17:18:55 +0200

    Initial commit

---
+++ part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author crist
+ */
+public class Fitbyte {
+    
+    private int age;
+    private double restingHeartRate;
+    
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age; 
+        this.restingHeartRate = restingHeartRate;
+    }
+    
+    public double targetHeartRate(double percentageOfMaximum) {
+        double maxHeartRate = 206.3 - (0.711 * this.age);
+        double targetHeartRate = (maxHeartRate - restingHeartRate) * (percentageOfMaximum) + restingHeartRate;
+        return targetHeartRate;
+    }
+    
+    
+    
+}
