20fa4ae997b76dcf0a18bab68b34e1a0b233192c
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-03-26 17:20:33 +0200

    Initial commit

---
+++ part05-Part05_07.NullPointerException/src/main/java/NielsBohr.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author crist
+ */
+public class NielsBohr {
+    
+    private int age;
+    
+    public NielsBohr(int age) {
+        this.age = age;
+    }
+    
+    public void growOlder() {
+        this.age++;
+    }
+    
+}
