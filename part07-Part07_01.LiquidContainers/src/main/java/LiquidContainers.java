a65c0450de61b690c5634ae83b9647f3f1049efd
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-03-26 17:42:33 +0200

    Initial commit

---
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+
+public class LiquidContainers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int first = 0;
+        int second = 0;
+
+        while (true) {  
+            
+            System.out.print("\n" + "First: " + first + "/100" + "\n" + "Second: " + second + "/100" + "\n");
+
+            String input = scan.nextLine();               
+            if (input.equals("quit")) {
+                break;
+            }       
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if(command.equals("add")) {
+                if(amount >= 0) {
+                    first = first + amount;
+                    if(first > 100) {
+                        first = 100;
+                    }
+                }              
+            }  
+            
+            if(command.equals("move")) {
+                if(amount > first) {
+                    second = second + first;
+                    first = 0;
+                    if(second > 100) {
+                        second = 100;
+                    }                        
+                } else {
+                    second = second + amount;
+                    first = first - amount;
+                    if(second > 100) {
+                        second = 100;
+                    } 
+                }               
+            }
+            
+            if(command.equals("remove")) {
+                second = second - amount;
+                if(second < 0) {
+                    second = 0;
+                }
+            }
+
+        }
+    }
+
+}
