4ec3802880d25fc71b91b2b2908893d73ef3522d
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-03-26 17:42:01 +0200

    Initial commit

---
+++ part06-Part06_13.Exercises/src/main/java/MainProgram.java
@@ -0,0 +1,16 @@
+
+public class MainProgram {
+    
+    public static void main(String args[]) {
+    
+        ExerciseManagement a = new ExerciseManagement();   
+        
+   
+    }
+    
+    public static int partsCompleted() {
+        return 0;
+    }
+
+    
+}
